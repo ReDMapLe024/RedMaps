f0e0541125268936449430a4252dfba9f342e86e
ReDMapLe024 <pfillion024@gmail.com>
2015-05-25 16:10:22 -0400

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,19 @@
 public class Tile{
 
+  public static final int BLOCKED = 0;
+  public static final int NORMAL = 1;
+  public static final int DOORWAY = 2;
+
+  private BufferedImage [] image;
+  private int tileType;
+
+  public Tile(Buffered [] image, int tileType ){
+    
+    this.image = image;
+    this.tileType = tileType;
+    
+  }
+  
+  public BufferedImage getImage(int index){ return image[index]; }
+  public int getTileType(){ return tileType; }
 }
