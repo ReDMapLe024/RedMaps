f7a2aacd7ef3b547fc17c18a2e534d9912729bdc
ReDMapLe024 <pfillion024@gmail.com>
2015-06-02 10:42:32 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,13 @@
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 
 public class Main{
     
-	
-	public Main(){
+	public static void main(String [] args){
 	    JFrame frame = new JFrame("RedMaps");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setSize(400, 300);
 	    frame.setLocationRelativeTo(null);
-	    JPanel panel = new JPanel();
+	    MainPanel mp = new MainPanel();
 	    panel.setLayout(null);
 	    frame.setVisible(true);
 	}
-	
-	public static void main(String [] args){
-    		Main main = new Main();
-  	}
 }
