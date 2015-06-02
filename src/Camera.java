2aff21fb5ec8fa010a7714467e486617ced770bb
ReDMapLe024 <pfillion024@gmail.com>
2015-06-02 12:51:14 -0400

    Update Camera.java

--- src/Camera.java
+++ src/Camera.java
@@ -1,3 +1,25 @@
 public class Camera{
-
+  private leftX;
+  private leftY;
+  private x;
+  private y;
+  
+  public Camera(){
+    x = 0;
+    y = 0;
+  }
+  
+  public setCameraPosition(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  
+  public getX(){
+    return x;
+  }
+  
+  public getY(){
+    return y;
+  }
+  
 }
